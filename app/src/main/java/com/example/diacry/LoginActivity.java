3947f1713adbfa40f55bafda496dcb6c339b4cb9
RamintaMis <raminta.misiunaite@gmail.com>
2022-01-31 14:00:24 +0000

    added back buttons and titles to register and login

--- app/src/main/java/com/example/diacry/LoginActivity.java
+++ app/src/main/java/com/example/diacry/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.diacry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("Log in");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- app/src/main/java/com/example/diacry/registerActivity.java
+++ app/src/main/java/com/example/diacry/registerActivity.java
@@ -10,5 +10,8 @@ public class registerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().setTitle("Register");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
